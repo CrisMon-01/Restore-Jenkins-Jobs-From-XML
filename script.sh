9da308bde52a59eb093ee07e4388ff907cd93213
Cristian Monti <cristianmonti1995@gmail.com>
2019-03-07 16:20:30 +0100

    update var name

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for i in *.xml
+for job in *.xml
 do
-    ansible-vault view $i > config.xml
-    NOME="$(basename $i .xml)"
+    ansible-vault view $job > config.xml
+    NOME="$(basename $job .xml)"
     URL=https://urlserver2/createItem?name=$NOME
     curl -s -k -XPOST $URL -u user:accesstoken --data-binary @config.xml -H "Content-Type:text/xml"
     rm config.xml
